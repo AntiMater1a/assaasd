9b0f47551c587fef74a6be7fb3377ae9228db309
AntiMater1a <aidan9135@gmail.com>
2025-05-24 21:59:11 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,215 @@
+const treasures = [
+            {
+      note: "One, two, three, a bear lies within me. Head north, you will see.",
+      location: "Cedar Rapids",
+      images: [
+	  "https://cdn.discordapp.com/attachments/1375778485414400080/1375781256905625731/image.png?ex=6832efa4&is=68319e24&hm=252988bfb96cdba7ff357bb8e63377f70ce6d468c0d537355f2c6007e142a31d& ",
+        "https://cdn.discordapp.com/attachments/1375778485414400080/1375783614775562372/image.png?ex=6832f1d6&is=6831a056&hm=70436bd493b0dd42e8bc58bad8764fd6bdd91f32010b88ed772d1bf39d13829f& ",
+        "https://cdn.discordapp.com/attachments/1375778485414400080/1375783626494709770/image.png?ex=6832f1d9&is=6831a059&hm=4df82f4c382b32d36187120f870b08488525025454063e5fa38c7f5ea0ec1c35& ",
+        "https://cdn.discordapp.com/attachments/1375778485414400080/1375783632547086446/image.png?ex=6832f1da&is=6831a05a&hm=2169965b8e03ad7abbed3e5963eab7423e5c4dcea481b00f9eb5d394ca3ca658& ",
+      ],
+    },
+    {
+      note: "Heehee! A riddle for thee! A barrel, a bottle, and a trough filled full! No feed for the horses' athirst... But a chest! Waitin' fer yee!",
+      location: "Delores Ranch",
+      images: [
+        "https://cdn.discordapp.com/attachments/1375778485414400080/1375784135683211345/image.png?ex=6832f252&is=6831a0d2&hm=c67edffb7c5343ca6c44e1702150014569aba5cd4fef9b02428e25145f2abfb8& ",
+        "https://cdn.discordapp.com/attachments/1375778485414400080/1375784180083982509/image.png?ex=6832f25d&is=6831a0dd&hm=7b3e0b498a89ac22e4cb97f36850329a9648406dd905b1d3ea0fdde0d2ef6b86& ",
+        "https://cdn.discordapp.com/attachments/1375778485414400080/1375784220198436874/image.png?ex=6832f266&is=6831a0e6&hm=e64a4bb0b6d0dbb02901e1ab438ad5088327a9fa97da8efbe7cd74e916c390ae& ",
+        "https://cdn.discordapp.com/attachments/1375778485414400080/1375784230432542882/image.png?ex=6832f269&is=6831a0e9&hm=e878050086a4a0792da37758479e1e4d2ce5b0268059a38e2e79a9cb6aaabc12& ",
+      ],
+    },
+    {
+      note: "To my future self. I done stashed my belongings, 'bout ten paces back younger, near that old west-most facin' felled log what's perched atop them Pinewood Hills.",
+      location: "Pinewood Hills",
+      images: [
+        "https://cdn.discordapp.com/attachments/1375778485414400080/1375785037542461520/image.png?ex=6832f329&is=6831a1a9&hm=66c2ff7b2b4ca5d59f6507ae5371aafddfaf46e283f03b0439cab506c6dc793b& ",
+        "https://cdn.discordapp.com/attachments/1375778485414400080/1375785095897677944/image.png?ex=6832f337&is=6831a1b7&hm=7a87952a38f4a2264ac381a141e4301b0d88aab0a1869c7d85df7ed89078af15& ",
+        "https://cdn.discordapp.com/attachments/1375778485414400080/1375785131247276042/image.png?ex=6832f340&is=6831a1c0&hm=32235e2f076655518c6dced0610602f6fc6908a70933b12610c8521887138388& ",
+        "https://cdn.discordapp.com/attachments/1375778485414400080/1375785142395600946/image.png?ex=6832f342&is=6831a1c2&hm=479543c49225481d9abce17d541aedb4e08b43549fbab7bd3cf2caeeae562c9f& ",
+      ],
+    },
+    {
+      note: "Yarrr! Listen up, pardner, to the one who's readin' this, my most prized stash of trinkets can be found on a wee rocky isle.",
+      location: "Pinewood Hills",
+      images: [
+        "https://cdn.discordapp.com/attachments/1375778485414400080/1375785492360200252/image.png?ex=6832f396&is=6831a216&hm=d9bce3635bfa709d196813fd2e4681062b85d9d58343f264397ae967c84f836b& ",
+        "https://cdn.discordapp.com/attachments/1375778485414400080/1375785550803370115/image.png?ex=6832f3a4&is=6831a224&hm=ee4f3238b0ad211e8eae8aca8761933e862e2d0e35f55a2ea0392f126f993a46& ",
+        "https://cdn.discordapp.com/attachments/1375778485414400080/1375785566033154078/image.png?ex=6832f3a7&is=6831a227&hm=380d30eafaa3be1799ee7a4ad8da9d5c6ffd492412283882f406dea4ca2ab969& ",
+        "https://cdn.discordapp.com/attachments/1375778485414400080/1375785570013544468/image.png?ex=6832f3a8&is=6831a228&hm=76237aa9a17ff1359c86ac44ccba3a4f1f25f441a897761ddccab835ed2f6193& ",
+      ],
+    },
+    {
+      note: "For Peety, Near he who creates a lake, I done hid my treasure in stolen land for heaven's sake. For months, his roar kept me wide awake, but my belongings remain unscathed beneath golden leaves, make no mistake.",
+      location: "Silvertown",
+      images: [
+        "https://cdn.discordapp.com/attachments/1375778485414400080/1375786123510415390/image.png?ex=6832f42c&is=6831a2ac&hm=3fced41b66060fe6569a8810a87a42b43f10f2e0d4248994d9fc7e1ff4725a06& ",
+        "https://cdn.discordapp.com/attachments/1375778485414400080/1375786171220627456/image.png?ex=6832f438&is=6831a2b8&hm=a82a52d12895a11a09061ef142d7f0cc9b4ab59cd7f3ef14546c5fb286535ce3& ",
+        "https://cdn.discordapp.com/attachments/1375778485414400080/1375786181408587786/image.png?ex=6832f43a&is=6831a2ba&hm=cff1a04926ea5e019e2417d6c071f893937682dba966e39309dcded615286140& ",
+        "https://cdn.discordapp.com/attachments/1375778485414400080/1375786187670814750/image.png?ex=6832f43c&is=6831a2bc&hm=2ce56f77607597b4fcae4e5182cbce70d63fa84e922a5929698a4ef92ed7b3d0& ",
+      ],
+    },
+    {
+      note: "For a hero, From a path of justice, guarded by an old warrior's favored stone, and the love of mum', my belongings lay down to rest.",
+      location: "Bronze city",
+      images: ["https://cdn.discordapp.com/attachments/1375778485414400080/1375793077062598696/image.png?ex=6832faa6&is=6831a926&hm=fb7b18ad4cd8ec9b5e11457c2cc4fb2431590d85cdeb694f2b8ebefae48b0070&", "https://cdn.discordapp.com/attachments/1375778485414400080/1375793105160110141/image.png?ex=6832faad&is=6831a92d&hm=19a67587508cf32e9d3c29ccb210feded3b172249bf12e9dd397995311211251&", "https://cdn.discordapp.com/attachments/1375778485414400080/1375793123694874695/image.png?ex=6832fab1&is=6831a931&hm=c4a48bf8e75bab37e4f4e69f11833872251155a7598735dae486d3b3843e7bf9&", "https://cdn.discordapp.com/attachments/1375778485414400080/1375793145295409222/image.png?ex=6832fab6&is=6831a936&hm=f3048e12919090f2ec6e6428838593deff8afffde608c04d4dae34b7be878a2f&"],
+    },
+    {
+      note: "Ordained seeker, As the oak casts a shade, the blind lady watches in disdain.",
+      location: "Bronze city",
+      images: ["https://cdn.discordapp.com/attachments/1375778485414400080/1375793606992072725/image.png?ex=6832fb24&is=6831a9a4&hm=2b0c7b52cc90319c8d0d9d8495e521beb9a70fb3a72174e8d2e646ef70b5455a&", "https://cdn.discordapp.com/attachments/1375778485414400080/1375793645919408199/image.png?ex=6832fb2e&is=6831a9ae&hm=877a2bebc0d99a27d2896d1df560d7dcde73086ec8bb04d1a27a6321f2317934&", "https://cdn.discordapp.com/attachments/1375778485414400080/1375793683147919380/image.png?ex=6832fb37&is=6831a9b7&hm=6a895bed86064e51b5a7d7e8096b4b51e9bffed5caaa5dc6185c82713a585a51&", "https://cdn.discordapp.com/attachments/1375778485414400080/1375793706661056542/image.png?ex=6832fb3c&is=6831a9bc&hm=a5377c57bf5ad1b798d45dd5a51eb9793f3932d470994b05078e6cc82c783f89&"],
+    },
+    {
+      note: "Me and my pardner was just a-driftin' 'cross the range when we happened on this pretty little bridge. We forded that there crick and chowed down on some fine grub under a shady ol' tree. If I ever get my hands on enough coin, I'm aimin' to buy this patch and raise a home here. 'Til that day comes, I reckon I'll mark the spot with this here box.",
+      location: "cedar rapids",
+      images: ["https://cdn.discordapp.com/attachments/1375778485414400080/1375797948310491186/image.png?ex=6832ff2f&is=6831adaf&hm=4e703b95e59e1954ae912fe8d65f822c28bc3caf7c9f6378571365635d2e7127&", "https://cdn.discordapp.com/attachments/1375778485414400080/1375797993084813372/image.png?ex=6832ff3a&is=6831adba&hm=23d7d204295256186eb0fb6174bd5ab8a4dc87ed35bf184f8db44d3377ea8a21&", "https://cdn.discordapp.com/attachments/1375778485414400080/1375798004371554314/image.png?ex=6832ff3d&is=6831adbd&hm=da3496ea40e23051ef1a16b02e765d47cf065efb02bf7a41eae8195e9c9bdc64&", "https://cdn.discordapp.com/attachments/1375778485414400080/1375798012609040454/image.png?ex=6832ff3f&is=6831adbf&hm=5cc3fd5878bc693c4c1d285d9ec9d6d474bb466af58d833409faaa43838981f4&"],
+    },
+    {
+      note: "His curiosity brought his doom and turned him into a monster. Now, stand in front of his legacy and dig, and your curiosity will bring you riches instead.",
+      location: "Delores runch",
+      images: ["https://cdn.discordapp.com/attachments/1375778485414400080/1375798229094105108/image.png?ex=6832ff72&is=6831adf2&hm=407cd261abeb82ef5c832187202da2d9ea97d99619180dc7ca807df5082240dc&", "https://cdn.discordapp.com/attachments/1375778485414400080/1375798274040266834/image.png?ex=6832ff7d&is=6831adfd&hm=af2c68edbd4afb0103c5b7cf200c39404f7bfaa1b428d18f95200f373f13b4f0&", "https://cdn.discordapp.com/attachments/1375778485414400080/1375798284030840872/image.png?ex=6832ff80&is=6831ae00&hm=383a74275f2a80a9cff33b4d2a584e61fc6d018fdd26289234386e9d94ca5abe&", "https://cdn.discordapp.com/attachments/1375778485414400080/1375798282311434281/image.png?ex=6832ff7f&is=6831adff&hm=cbc710eb4f071b0d415988bad27c0c1335ef154ccd2273a4a327204f8936af64&"],
+    },
+    {
+      note: "Pardner, yeah, you! Reckon my boss ain't never gonna give me a moment's peace if'n I don't finish scrubbin' these dishes afore sundown. Mind headin' to my break spot next to the back door and grabbing my stuff for me? Suppose I'll even let 'cha keep some of it.",
+      location: "Bronze city",
+      images: ["https://cdn.discordapp.com/attachments/1375778485414400080/1375798612285591602/image.png?ex=6832ffce&is=6831ae4e&hm=b6725ee332d7b73e03dbf0568f795b11d60a527597ecd87f211a586b7508c18a&", "https://cdn.discordapp.com/attachments/1375778485414400080/1375798644543852738/image.png?ex=6832ffd5&is=6831ae55&hm=298a9e89d94f38b1c6c4c9caeb6060cd392e619e5463200885c079ad0770d58b&", "https://cdn.discordapp.com/attachments/1375778485414400080/1375798663174946906/image.png?ex=6832ffda&is=6831ae5a&hm=1de4366592e1a079fbd0f466ebebab953dafb0dff3aaa5248351e4e3f94121fa&", "https://cdn.discordapp.com/attachments/1375778485414400080/1375798662739005490/image.png?ex=6832ffda&is=6831ae5a&hm=cbf9813a23edf5e129f940afc869388ae6964f9af5b1c417c54c355882f1be6e&"],
+    },
+    {
+      note: "",
+      location: "",
+      images: [""],
+    },
+    {
+      note: "",
+      location: "",
+      images: [""],
+    },
+    {
+      note: "",
+      location: "",
+      images: [""],
+    },
+    {
+      note: "",
+      location: "",
+      images: [""],
+    },
+    {
+      note: "",
+      location: "",
+      images: [""],
+    },
+    {
+      note: "",
+      location: "",
+      images: [""],
+    },
+    {
+      note: "",
+      location: "",
+      images: [""],
+    },
+    {
+      note: "",
+      location: "",
+      images: [""],
+    },
+    {
+      note: "",
+      location: "",
+      images: [""],
+    },
+    {
+      note: "",
+      location: "",
+      images: [""],
+    },
+    {
+      note: "",
+      location: "",
+      images: [""],
+    },
+];
+
+function findTreasure() {
+    const input = document.getElementById('noteInput').value.trim().toLowerCase();
+    const resultDiv = document.getElementById('result');
+    resultDiv.innerHTML = '';
+
+    if (!input) {
+        resultDiv.innerHTML = '<p style="color: var(--accent);">Введите текст записки.</p>';
+        return;
+    }
+
+    const matches = treasures.filter(t => t.note.toLowerCase().includes(input));
+    if (matches.length > 0) {
+        matches.forEach(match => {
+            let imagesHtml = '';
+            match.images.forEach((image, index) => {
+                imagesHtml += `<img src="${image}" alt="${match.location}" data-index="${index}" onclick="openModal('${image}')">`;
+            });
+            resultDiv.innerHTML += `
+                <div class="treasure-item">
+                    <h2>${match.location}</h2>
+                    <p><strong>Записка:</strong> ${match.note}</p>
+                    <div class="images">${imagesHtml}</div>
+                </div>
+            `;
+        });
+    } else {
+        resultDiv.innerHTML = '<p style="color: var(--accent);">Клады не найдены. Попробуйте другой текст.</p>';
+    }
+}
+
+// Функция для открытия модального окна
+function openModal(imageSrc) {
+    const modal = document.getElementById('modal');
+    const modalImage = document.getElementById('modalImage');
+    modalImage.src = imageSrc;
+    modal.classList.add('active');
+}
+
+// Закрытие модального окна при клике вне изображения
+document.getElementById('modal').addEventListener('click', function(event) {
+    if (event.target === this) {
+        this.classList.remove('active');
+    }
+});
+
+// Пасхалка
+const easterEgg = document.getElementById('easterEgg');
+let clickCount = 0;
+document.getElementById('noteInput').addEventListener('keydown', (event) => {
+    if (event.key === 'ArrowUp' && clickCount < 3) {
+        clickCount++;
+    } else {
+        clickCount = 0;
+    }
+    if (clickCount === 3) {
+        easterEgg.classList.add('active');
+        setTimeout(() => {
+            alert("🎉 Поздравляю! Ты нашёл пасхалку! 🎉");
+        }, 500);
+    }
+});
+
+easterEgg.addEventListener('click', () => {
+    alert("🎉 Спасибо, что нашли меня! 🎉");
+});
+
+// Генерация частиц
+const particlesContainer = document.getElementById('particles');
+for (let i = 0; i < 50; i++) {
+    const particle = document.createElement('div');
+    particle.classList.add('particle');
+    particle.style.left = `${Math.random() * 100}vw`;
+    particle.style.top = `${Math.random() * 100}vh`;
+    particle.style.animationDuration = `${Math.random() * 10 + 5}s`;
+    particlesContainer.appendChild(particle);
+}
+
+// Плавающий клад
+const floatingTreasure = document.getElementById('floatingTreasure');
+floatingTreasure.addEventListener('click', () => {
+    alert("💎 Этот клад спрятан специально для тебя! 💎");
+});
